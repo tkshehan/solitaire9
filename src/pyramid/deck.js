284ac5f6928eb0ef3ee429775dc82e7349cef368
Thomas Shehan <thomas.shehan88@gmail.com>
2018-11-02 16:42:22 -0400

    add card deck and pyramid solitair engine

---
+++ src/pyramid/deck.js
@@ -0,0 +1,72 @@
+class Deck {
+  constructor() {
+    this.deck = [];
+  }
+
+  reset() {
+    const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+    // 1 is Ace, 11 is Jack, 12 is Queen, 13 is King
+    const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+    suits.forEach((suit) => {
+      values.forEach((value) => {
+        this.deck.push({
+          value,
+          suit,
+        });
+      });
+    });
+  }
+
+  shuffle() {
+    const {deck} = this;
+    let m = deck.length;
+    let i;
+
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      [deck[m], deck[i]] = [deck[i], deck[m]];
+    }
+    return this;
+  }
+
+  deal(amount = 1) {
+    if (amount === 1 ) return this.deck.pop();
+
+    const cards = [];
+    for (let i = 0; i < amount; i++) {
+      if (this.deck.length === 0) return cards;
+      cards.push(this.deck.pop());
+    }
+    return cards;
+  }
+
+  add(cards) {
+    this.deck = [...this.deck, ...cards];
+  }
+}
+
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  add(card) {
+    this.stack = [...this.stack, card];
+  }
+
+  deal(amount = 1) {
+    const cards = [];
+    for (let i = 0; i < amount; i++) {
+      if (this.stack.length === 0) return cards;
+      cards.push(this.stack.pop());
+    }
+    return cards;
+  }
+
+  reset() {
+    this.stack = [];
+  }
+}
+
+module.exports = {Deck, Stack};
---
+++ src/pyramid/pyramid.js
@@ -0,0 +1,83 @@
+const {Deck, Stack} = require('./deck');
+
+class Pyramid {
+  constructor() {
+    this.initialize();
+    this.setup();
+  }
+
+  initialize() {
+    this.pyramid = [
+      [...Array(1)],
+      [...Array(2)],
+      [...Array(3)],
+      [...Array(4)],
+      [...Array(5)],
+      [...Array(6)],
+    ];
+    this.stock = new Deck();
+    this.stock.reset();
+    this.stock.shuffle();
+
+    this.pile = new Stack();
+    this.discard = new Stack();
+    this.reserve = [...Array(6)];
+    this.topCard = {};
+  }
+
+  setup() {
+    this.pyramid = this.pyramid.map((row) => {
+      return row.map(() => this.stock.deal());
+    });
+    this.pyramid[5].forEach((card) => card.active = true);
+    this.reserve = this.reserve.map(() =>
+      Object.assign(this.stock.deal(), {active: true})
+    );
+    this.topCard = this.stock.deal();
+    this.topCard.active = true;
+  }
+
+  draw() {
+    this.topCard.active = false;
+    this.pile.add(this.topCard);
+
+    this.topCard = this.stock.deal();
+    this.topCard.active = true;
+  }
+
+  isMatch(card1, card2 = {value: 0, active: true}) {
+    // Check if both cards are active and equal to 13;
+    if (!(card1.active && card2.active)) return false;
+    if (card1.value + card2.value !== 13) return false;
+
+    this.discard.add([Object.assign(card1), Object.assign(card2)]);
+    card1 = false;
+    card2 = false;
+
+    // Check if the topCard was used in the match
+    // Replace it with the last card put in the pile if so
+    if (this.topCard === false) {
+      this.topCard = this.pile.draw();
+      this.topCard.active = true;
+    }
+
+    this.findActive();
+    return true;
+  }
+
+  findActive() {
+    for (let i = 0; i < 5; i++) {
+      this.pyramid[i].forEach((card, j) => {
+        if (
+          card !== false
+          && this.pyramid[i+1][j] === false
+          && this.pyramid[i+1][j+1] === false
+        ) {
+          card.active = true;
+        }
+      });
+    }
+  }
+}
+
+module.exports = Pyramid;
