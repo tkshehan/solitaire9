32430ba84f70caee4f2104b2780f6c041a9a9721
Thomas Shehan <thomas.shehan88@gmail.com>
2018-11-08 12:45:01 -0500

    add links to navbar

--- src/components/main.js
+++ src/components/main.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {BrowserRouter as Link, Route} from 'react-router-dom';
+import {BrowserRouter as Switch, Link, Route} from 'react-router-dom';
 
 import Landing from './landing';
 
 export class Main extends React.Component {
   render() {
     return (
-      <main role="main">
-        <header role="banner">
-          <h1><Link to="/">{'Solitaire 9'}</Link></h1>
-        </header>
+      <Switch>
+        <main role="main">
+          <header role="banner">
+            <h1><Link to="/">{'Solitaire 9'}</Link></h1>
+          </header>
 
-        {/* <Route exact path="/" component={Landing} />
-        <Route exact path="/scores" />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/scores" />
 
-        <Route path="/game" />
-        <Route exact path="/profile/:profileId" />
+          <Route path="/game" />
+          <Route exact path="/profile/:profileId" />
 
-        <Route exact path="/login" />
-        <Route exact path="/signup" />
-        <Route exact path="/signout" /> */}
-      </main>
+          <Route exact path="/login" />
+          <Route exact path="/signup" />
+          <Route exact path="/signout" />
+        </main>
+      </Switch>
     );
   }
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import {BrowserRouter as Switch, Link} from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
-    // eslint-disable-next-line
-    <nav role="navigation">
-      <a href="/rules">Rules</a>
-      <a href="/game">Play</a>
-      <a href="">Profile/Login</a>
-      <a href="/scores">High Scores</a>
-    </nav>
+    <Switch>
+      {// eslint-disable-next-line
+        <nav role="navigation">
+          <Link to="/rules">Rules</Link>
+          <Link to="/game">Play</Link>
+          <Link to="">Profile/Login</Link>
+          <Link to="/scores">High Scores</Link>
+        </nav>
+      }
+    </Switch>
   );
 }
